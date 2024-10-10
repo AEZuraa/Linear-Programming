0606340ab9e4cbdff7fd6ce0b671f56e8d557a71
XriXis <i.lobazov@innopolis.university>
2024-10-11 00:39:05 +0300

    Documentation improvements

--- src/DoublePreciseComparator.java
+++ src/DoublePreciseComparator.java
@@ -1,7 +1,12 @@
 import java.util.Comparator;
 
+/** Comparator, used when doubles should be compared with respect to some accuracy ((a==b) == (abs(a-b) < e)) */
 public class DoublePreciseComparator implements Comparator<Double> {
     double accuracy;
+    /**
+     * Comparator, used when doubles should be compared with respect to some accuracy ((a==b) == (abs(a-b) < e))
+     * @param accuracy e
+     */
     public DoublePreciseComparator(double accuracy){
         this.accuracy = accuracy;
     }
