3565b2952c37b0c19e55cdc3c330094aaeaada00
artisaaep <104840798+artisaaep@users.noreply.github.com>
2024-10-10 12:53:00 +0300

    added max and min check

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws ExecutionControl.NotImplementedException {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 0 for minimization or 1 for maximization");
+        int flag = scanner.nextInt();
+        while (flag != 0 && flag != 1) {
+            System.out.println("Enter 0 for minimization or 1 for maximization");
+            flag = scanner.nextInt();
+        }
         Vector objectiveFunction = ColumnVector.scan(scanner);
         System.out.println("---\n");
         Matrix constrains = Matrix.scan(scanner);
@@ -13,7 +19,12 @@ public class Main {
         Vector rightHandSide = RowVector.scan(scanner);
         System.out.println("---\n");
         try {
-            SimplexMatrix solution = new SimplexMatrix(objectiveFunction, constrains, rightHandSide);
+            SimplexMatrix solution;
+            if (flag == 0) {
+                solution = new SimplexMatrix(objectiveFunction, constrains, rightHandSide, false);
+            } else {
+                solution = new SimplexMatrix(objectiveFunction, constrains, rightHandSide);
+            }
             while (!solution.iteration()){}
             System.out.println("Values of variables in optimal solution:\n" + solution.getObjectiveFunction());
             System.out.println("Maximum value of the objective function:\n" + solution.getObjectiveFunctionValue());
