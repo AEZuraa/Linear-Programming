87b914cf5a08c84ffc2d690a6750db058d3deac1
artisaaep <104840798+artisaaep@users.noreply.github.com>
2024-11-06 13:27:09 +0300

    vogel's approximation added

--- src/Node.java
+++ src/Node.java
@@ -1,6 +1,9 @@
 public class Node {
     int row, col;
     boolean rowTaken;
+    public Node(int r, int c){
+        row = r; col = c;
+    }
     public Node(int r, int c, boolean who){
         row = r; col = c; rowTaken = who;
     }
