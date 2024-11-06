721788614a4a93a19aed35c3994a403dcf5c166b
XriXis <i.lobazov@innopolis.university>
2024-11-06 13:27:13 +0300

    Main algorithm of transportation problem written

--- src/TransportationModel.java
+++ src/TransportationModel.java
@@ -1,3 +1,5 @@
+import Exceptions.ImproperConversionException;
+
 import java.util.ArrayList;
 
 public class TransportationModel {
@@ -5,5 +7,45 @@ public class TransportationModel {
     protected Vector demand;
     protected Vector supply;
     protected ArrayList<Node> taken;
+
+    Chooser method;
+
+    public TransportationModel(Matrix costs,
+                               Vector demand,
+                               Vector supply,
+                               Chooser approximationMethod
+    ) throws ImproperConversionException {
+        if (costs.getRows() != supply.size() || costs.getColumns() != demand.size()) {
+            throw new ImproperConversionException("Wrong input data: demand consist from "
+                    + demand.size() + " items; supply from "
+                    + supply.size()
+                    + " items; \nWhile costs matrix is (" + costs.getRows() + "x" + costs.getColumns() + ")");
+        }
+        this.costs = costs;
+        this.demand = demand;
+        this.supply = supply;
+        taken = new ArrayList<>();
+        method = approximationMethod;
+    }
+
+    public double solve() {
+        while (taken.size() == Math.max(costs.getRows(), costs.getColumns())) {
+            iteration();
+        }
+        double res = 0;
+        for (Node item : taken) {
+            res += costs.get(item.row, item.col) * item.provided;
+        }
+        return res;
+    }
+
+    public void iteration() {
+        Node item = method.choose(this);
+        double provided = Math.min(supply.get(item.row), demand.get(item.col));
+        demand.set(item.col, demand.get(item.col) - provided);
+        supply.set(item.row, supply.get(item.row) - provided);
+        item.setProvided(provided);
+        item.setRowTaken(supply.get(item.row) <= demand.get(item.col));
+    }
 }
 
