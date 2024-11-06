7bffdd813d6410b74ab96e36985bed44ccfd4b3a
XriXis <i.lobazov@innopolis.university>
2024-11-06 14:01:01 +0300

    Make IO

--- src/OptimizationMode.java
+++ src/OptimizationMode.java
@@ -1,9 +1,12 @@
 // Enum representing the optimization modes for a problem
 public enum OptimizationMode {
-    MAX(-1), MIN(1);
+    MAX(-1, "Maximum"), MIN(1, "Minimum");
 
     public final int factor;
-    OptimizationMode(int factor) {
+    public final String fullName;
+
+    OptimizationMode(int factor, String name) {
         this.factor = factor;
+        fullName = name;
     }
 }
