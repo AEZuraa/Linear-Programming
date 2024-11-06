cf46d481cf723193ed5cf106c16c219da2ce0387
AEZuraa <aezuraa@yandex.ru>
2024-11-06 17:03:49 +0300

    Russel Approximation method

--- src/Node.java
+++ src/Node.java
@@ -1,7 +1,6 @@
 public class Node {
     int row, col;
-    boolean rowTaken;
-    public Node(int r, int c, boolean who){
-        row = r; col = c; rowTaken = who;
+    public Node(int r, int c){
+        row = r; col = c;
     }
 }
---
+++ src/RusselChooser.java
@@ -0,0 +1,60 @@
+public class RusselChooser implements TodoNameThis {
+    @Override
+    public Node choose(TransportationModel object) {
+        double[] rowMaximum = new double[object.costs.getRows()];
+        double[] columnMaximum = new double[object.costs.getColumns()];
+        for (int i = 0; i < object.costs.getRows(); i++) {
+            double max = Double.MIN_VALUE;
+            for (int j = 0; j < object.costs.getColumns(); j++) {
+                double currentCost = object.costs.get(i, j);
+                if (currentCost > max) {
+                    max = currentCost;
+                }
+            }
+            rowMaximum[i] = max;
+        }
+
+        for (int j = 0; j < object.costs.getColumns(); j++) {
+            double max = Double.MIN_VALUE;
+            for (int i = 0; i < object.costs.getRows(); i++) {
+                double currentCost = object.costs.get(i, j);
+                if (currentCost > max) {
+                    max = currentCost;
+                }
+            }
+            columnMaximum[j] = max;
+        }
+
+        Matrix delta = new Matrix(object.costs.getRows(), object.costs.getColumns());
+
+        for (int i = 0; i < object.costs.getRows(); i++) {
+            for (int j = 0; j < object.costs.getColumns(); j++) {
+                double currentCost = object.costs.get(i, j);
+                double result = currentCost - rowMaximum[i] - columnMaximum[j];
+                delta.set(i, j, result);
+            }
+        }
+
+        double minValue = Double.MAX_VALUE;
+        int minRow = -1;
+        int minCol = -1;
+
+        for (int i = 0; i < delta.getRows(); i++) {
+            for (int j = 0; j < delta.getColumns(); j++) {
+                double value = delta.get(i, j);
+                if (value < minValue) {
+                    minValue = value;
+                    minRow = i;
+                    minCol = j;
+                }
+            }
+        }
+
+        return new Node(minRow, minCol);
+    }
+
+    @Override
+    public String toString() {
+        return "Russel Approximation";
+    }
+}
